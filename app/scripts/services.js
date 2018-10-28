4644272ac458e3063e9b58361502c2edc75cbe04
anstif <stifurak@gmail.com>
2018-10-28 11:49:04 +0200

    all

---
+++ app/scripts/services.js
@@ -0,0 +1,153 @@
+var services = [
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone 4 / 4s',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone 5 / 5S / SE',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone 6 / 6+',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone 7 / 7+',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone 8 / 8+',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+  [{
+      img: 'service-1.jpg',
+      title: 'iPhone X',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-2.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-3.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    },
+    {
+      img: 'service-4.jpg',
+      title: 'Title',
+      time: 'Сроки ремонта: 5-7 дней',
+      price: '1500 руб.'
+    }
+  ],
+
+]
